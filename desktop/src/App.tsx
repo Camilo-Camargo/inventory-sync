1497774017ee67664894680f7f98617efbfe9ca8
camilo-camargo <camilocamargo49@gmail.com>
2024-04-13 23:48:07 -0500

    feat: add desktop tauri app

---
+++ desktop/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh',display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'red' }}>
+      <h1>Inventory Sync</h1>
+    </div>
+  );
+}
+
+export default App;
